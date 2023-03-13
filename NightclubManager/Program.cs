9348cd29ac36671c4473ac6efc98d31522c08801
tigerros <tigerros.gh@gmail.com>
2023-03-13 21:02:48 +0100

    Update instructions.readme and some solution options (also README)

--- NightclubManager/Program.cs
+++ NightclubManager/Program.cs
@@ -1,96 +1,110 @@
-﻿using Sandbox.ModAPI.Ingame;
-using System;
-using System.Collections.Generic;
-using VRage.Game.ModAPI.Ingame.Utilities;
+﻿namespace IngameScript
+{
+	using Sandbox.ModAPI.Ingame;
+	using System;
+	using System.Collections.Generic;
+	using VRage.Game.ModAPI.Ingame.Utilities;
+	using SEKit.Extensions;
+	using SEKit.Classes;
+	using SEKit.Enums;
 
-namespace IngameScript {
-	class Command {
-		public string Name { get; set; }
-		public string Description { get; set; }
-		public Action Action { get; set; }
-
-		public Command(string name, string description, Action action) {
-			Name = name;
-			Description = description;
-			Action = action;
-		}
-
-		public override string ToString() {
-			return $"{Name} - {Description}";
-		}
-	}
-
-	partial class Program : MyGridProgram {
+	class Program : MyGridProgram
+	{
+		// Since this is not minified, adding "private" would take up space
+		// ReSharper disable ArrangeTypeMemberModifiers
 		#region mdk preserve
+		// The nightclub exhaust group
 		const string NightclubExhaustGroupName = "Nightclub Exhausts";
-		const string NightclubRotatingLightGroupName = "Nightclub Rotating Lights";
+		// The nightclub light group. The "NightclubNormalLight" should be in this group.
+		const string NightclubLightGroupName = "Nightclub Lights";
+		// The nightclub normal light. This light will be turned off during "party mode" and turned on otherwise.
+		// This is the last constant. Don't change anything below this!
 		const string NightclubNormalLightName = "Nightclub Normal Light";
-
 		#endregion
+		// ReSharper restore ArrangeTypeMemberModifiers
 
-		MyCommandLine _commandLine = new MyCommandLine();
-		Dictionary<string, Command> _commands = new Dictionary<string, Command>(StringComparer.OrdinalIgnoreCase);
-		bool isNightclubOn = false;
+		private readonly MyCommandLine _commandLine = new MyCommandLine();
+		private readonly Dictionary<string, Command> _commands = new Dictionary<string, Command>(StringComparer.OrdinalIgnoreCase);
+		private bool _isNightclubOn;
 
-		public Program() {
-			_commands["nightclub"] = new Command("nightclub", "Turn the nightclub party mode on/off.", Nightclub);
+		public Program()
+		{
+			_commands["nightclub"] = new Command("nightclub", "Toggles the nightclub \"party mode\" (rotating lights and exhaust pipes).", Nightclub);
 		}
 
-		private void NightclubOff(List<IMyTerminalBlock> exhausts, List<IMyReflectorLight> lights) {
-			for (int ei = 0; ei < exhausts.Count; ei++) {
+		private void NightclubOff(List<IMyTerminalBlock> exhausts, List<IMyLightingBlock> lights)
+		{
+			for (int ei = 0; ei < exhausts.Count; ei++)
+			{
 				exhausts[ei].ApplyAction("OnOff_Off");
 			}
 
-			for (int rli = 0; rli < lights.Count; rli++) {
-				IMyReflectorLight light = lights[rli];
+			for (int rli = 0; rli < lights.Count; rli++)
+			{
+				IMyLightingBlock light = lights[rli];
 
 				light.Enabled = light.CustomName == NightclubNormalLightName;
 			}
 		}
 
-		private void NightclubOn(List<IMyTerminalBlock> exhausts, List<IMyReflectorLight> lights) {
-			for (int ei = 0; ei < exhausts.Count; ei++) {
+		private void NightclubOn(List<IMyTerminalBlock> exhausts, List<IMyLightingBlock> lights)
+		{
+			for (int ei = 0; ei < exhausts.Count; ei++)
+			{
 				exhausts[ei].ApplyAction("OnOff_On");
 			}
 
-			for (int rli = 0; rli < lights.Count; rli++) {
-				IMyReflectorLight light = lights[rli];
+			for (int rli = 0; rli < lights.Count; rli++)
+			{
+				IMyLightingBlock light = lights[rli];
 
 				light.Enabled = light.CustomName != NightclubNormalLightName;
 			}
 		}
 
-		public void Nightclub() {
+		public void Nightclub()
+		{
 			IMyBlockGroup exhaustGroup = GridTerminalSystem.GetBlockGroupWithName(NightclubExhaustGroupName);
 			var exhausts = new List<IMyTerminalBlock>();
 			exhaustGroup.GetBlocks(exhausts);
 
-			IMyBlockGroup rotatingLightGroup = GridTerminalSystem.GetBlockGroupWithName(NightclubRotatingLightGroupName);
-			var rotatingLights = new List<IMyReflectorLight>();
-			rotatingLightGroup.GetBlocksOfType(rotatingLights);
+			IMyBlockGroup lightGroup = GridTerminalSystem.GetBlockGroupWithName(NightclubLightGroupName);
+			var lights = new List<IMyLightingBlock>();
+			lightGroup.GetBlocksOfType(lights);
 
-			if (isNightclubOn) {
-				NightclubOff(exhausts, rotatingLights);
-			} else {
-				NightclubOn(exhausts, rotatingLights);
+			if (_isNightclubOn)
+			{
+				Me.WriteMessageToSurface(TextMessage.NewLine("Turning party mode off.", TextMessageType.Info), ProgrammableBlockSurfaceType.Display);
+				NightclubOff(exhausts, lights);
+			}
+			else
+			{
+				Me.WriteMessageToSurface(TextMessage.NewLine("Turning party mode on.", TextMessageType.Info), ProgrammableBlockSurfaceType.Display);
+				NightclubOn(exhausts, lights);
 			}
 
-			isNightclubOn = !isNightclubOn;
+			_isNightclubOn = !_isNightclubOn;
 		}
 
-		public void Main(string argument, UpdateType updateSource) {
-			if (_commandLine.TryParse(argument)) {
-				Command command;
+		public void Main(string argument, UpdateType updateSource)
+		{
+			if (!_commandLine.TryParse(argument)) return;
 
-				string commandString = _commandLine.Argument(0);
+			Command command;
 
-				if (commandString == null) {
-					Echo($"No command specified. Available commands: {string.Join(",", _commands.Values)}");
-				} else if (_commands.TryGetValue(commandString, out command)) {
-					command.Action();
-				} else {
-					Echo($"Unknown command {commandString}");
-				}
+			string commandString = _commandLine.Argument(0);
+
+			if (commandString == null) 
+			{
+				Echo($"No command specified. Available commands: {string.Join(",", _commands.Values)}");
+			}
+			else if (_commands.TryGetValue(commandString, out command))
+			{
+				command.Action();
+			}
+			else
+			{
+				Echo($"Unknown command {commandString}");
 			}
 		}
 	}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Manages the nightclub of Haluz (ship I'm building with friends).
 The contents of the file `ProgrammableBlockOutput.txt` should be pasted into a programmable block in-game.
+
+Instructions can be found in the `ProgrammableBlockOutput.txt`.
